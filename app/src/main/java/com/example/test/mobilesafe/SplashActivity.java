8019b52e6f0d7b3aaae77e1d66ee1b2de7ba77dd
michael <neo21anderson@gmail.com>
2015-08-22 13:42:05 +0800

    create splash activity
    create splash activity, with animation and version name

---
+++ app/src/main/java/com/example/test/mobilesafe/SplashActivity.java
@@ -0,0 +1,70 @@
+package com.example.test.mobilesafe;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    private LinearLayout ll_splash;
+    private TextView tv_splash_version;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
+        ll_splash = (LinearLayout) findViewById(R.id.ll_splash);
+
+        Animation animation = new AlphaAnimation(0f, 1f);
+        animation.setDuration(5000);
+        ll_splash.startAnimation(animation);
+
+        tv_splash_version.setText("Version " + getVersionName());
+    }
+
+    private String getVersionName() {
+        PackageManager packageManager = getPackageManager();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(this.getPackageName(), 0);
+            String versionName = packageInfo.versionName;
+            return versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "版本号未知";
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
