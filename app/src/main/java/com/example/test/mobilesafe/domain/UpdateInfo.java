1347c1258d9ea39550e545a176e3f87d17bd8a27
michael <neo21anderson@gmail.com>
2015-08-24 14:58:27 +0800

    create updateinfo class
    create updateinfo class

---
+++ app/src/main/java/com/example/test/mobilesafe/domain/UpdateInfo.java
@@ -0,0 +1,34 @@
+package com.example.test.mobilesafe.domain;
+
+/**
+ * Created by test on 8/24/2015.
+ */
+public class UpdateInfo {
+    private String version;
+    private String description;
+    private String apkurl;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getApkurl() {
+        return apkurl;
+    }
+
+    public void setApkurl(String apkurl) {
+        this.apkurl = apkurl;
+    }
+}
