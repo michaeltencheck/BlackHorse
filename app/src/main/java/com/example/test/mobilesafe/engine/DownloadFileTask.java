42191e8394e90ba726a18ae82d5089985594cbd2
michael <neo21anderson@gmail.com>
2015-08-26 13:32:19 +0800

    create a download class
    create a download class by using inputstream and fileoutputstream

---
+++ app/src/main/java/com/example/test/mobilesafe/engine/DownloadFileTask.java
@@ -0,0 +1,34 @@
+package com.example.test.mobilesafe.engine;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by test on 8/26/2015.
+ */
+public class DownloadFileTask {
+    public File getFile(String downloadPath, String filePath) throws Exception {
+        URL url = new URL(downloadPath);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.setConnectTimeout(8888);
+        if (httpURLConnection.getResponseCode() == 200) {
+            InputStream inputStream = httpURLConnection.getInputStream();
+            File file = new File(filePath);
+            FileOutputStream fos = new FileOutputStream(file);
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = inputStream.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
+            }
+            fos.flush();
+            fos.close();
+            inputStream.close();
+            return file;
+        }
+        return null;
+    }
+}
