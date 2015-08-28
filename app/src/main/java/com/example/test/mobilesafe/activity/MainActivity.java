5345fd1b9c35df5484bcded665be706edd351631
michael <neo21anderson@gmail.com>
2015-08-29 02:29:32 +0800

    implement long press item to change name
    implement long press item to change name

--- app/src/main/java/com/example/test/mobilesafe/activity/MainActivity.java
+++ app/src/main/java/com/example/test/mobilesafe/activity/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.test.mobilesafe.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.test.mobilesafe.R;
 import com.example.test.mobilesafe.adapter.MainUIAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+    private static final String TAG = "MainActivity";
     private GridView gv_main;
     private MainUIAdapter adapter;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +31,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         gv_main = (GridView) findViewById(R.id.gv_main);
         adapter = new MainUIAdapter(this);
+        sp = getSharedPreferences("config", MODE_PRIVATE);
         gv_main.setAdapter(adapter);
+        gv_main.setOnItemClickListener(this);
+        gv_main.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, final View view, int position, long id) {
+                if (position == 0) {
+                    final EditText et = new EditText(MainActivity.this);
+                    et.setHint("点击输入新名称");
+                    AlertDialog.Builder ab = new AlertDialog.Builder(MainActivity.this);
+                    ab.setTitle("更改名称");
+                    ab.setMessage("请输入新名称");
+                    ab.setView(et);
+                    ab.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String text = et.getText().toString().trim();
+                            if ("".equals(text)) {
+                                Toast.makeText(MainActivity.this,
+                                        "名称不能为空，请重新输入", Toast.LENGTH_LONG).show();
+                                return;
+                            } else {
+                                sp.edit().putString("changed name", text).commit();
+                                TextView tv = (TextView) view.findViewById(R.id.tv_functionText);
+                                tv.setText(text);
+                            }
+                        }
+                    });
+                    ab.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    ab.create().show();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -43,5 +92,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG, "点击位置是" + position);
+        switch (position) {
+            case 0:
+                Log.i(TAG, "进入手机防盗");
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/test/mobilesafe/adapter/MainUIAdapter.java
+++ app/src/main/java/com/example/test/mobilesafe/adapter/MainUIAdapter.java
@@ -1,6 +1,7 @@
 package com.example.test.mobilesafe.adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,10 +25,14 @@ public class MainUIAdapter extends BaseAdapter{
             R.drawable.widget07, R.drawable.widget08, R.drawable.widget01};
     private Context context;
     private LayoutInflater inflater;
+    private SharedPreferences sp;
+    private static ImageView imageView;
+    private static TextView textView;
 
     public MainUIAdapter(Context context) {
         this.context = context;
         inflater = LayoutInflater.from(context);
+        sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
     }
 
     @Override
@@ -49,10 +54,16 @@ public class MainUIAdapter extends BaseAdapter{
     public View getView(int position, View convertView, ViewGroup parent) {
         Log.i("aaaa", "getView" + position);
         View view = inflater.inflate(R.layout.main_item, null);
-        ImageView imageView = (ImageView) view.findViewById(R.id.iv_function);
-        TextView textView = (TextView) view.findViewById(R.id.tv_functionText);
+        imageView = (ImageView) view.findViewById(R.id.iv_function);
+        textView = (TextView) view.findViewById(R.id.tv_functionText);
         imageView.setImageResource(picID[position]);
-        textView.setText(names[position]);
+        if (position == 0) {
+            String chagedName = sp.getString("changed name", "");
+            Log.i("MainActivity", chagedName);
+            textView.setText(chagedName);
+        } else {
+            textView.setText(names[position]);
+        }
         return view;
     }
 }
