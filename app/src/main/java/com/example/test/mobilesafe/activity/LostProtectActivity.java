09733bb91c2ffc5d2aebd600666ee359e01b6654
michael <neo21anderson@gmail.com>
2015-08-31 18:58:27 +0800

    make a judgement
    make a judgement to judge whether the password has been set up

--- app/src/main/java/com/example/test/mobilesafe/activity/LostProtectActivity.java
+++ app/src/main/java/com/example/test/mobilesafe/activity/LostProtectActivity.java
@@ -4,14 +4,22 @@ import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.test.mobilesafe.R;
 
-public class LostProtectActivity extends AppCompatActivity {
+public class LostProtectActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = "LostProtectActivity";
     private SharedPreferences sp;
+    private EditText pwd;
+    private EditText pwdConfirm;
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +40,20 @@ public class LostProtectActivity extends AppCompatActivity {
         /*Dialog dialog = new Dialog(this, R.style.MyDialog);
         dialog.setContentView(R.layout.pwd_not_setup);
         dialog.show();*/
-        Dialog dialog = new Dialog(this);
+        dialog = new Dialog(this);
         dialog.setCanceledOnTouchOutside(false);
         dialog.setTitle("设置密码");
-        dialog.setContentView(R.layout.pwd_not_setup);
+        View view = View.inflate(this, R.layout.pwd_not_setup, null);
+        pwd = (EditText) view.findViewById(R.id.et_pwd);
+        pwdConfirm = (EditText) view.findViewById(R.id.et_pwdConfirm);
+        Button ok = (Button) view.findViewById(R.id.bt_ok);
+        Button cancel = (Button) view.findViewById(R.id.bt_cancel);
+        ok.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+        dialog.setContentView(view);
+//        dialog.setContentView(R.layout.pwd_not_setup);
         dialog.show();
+
 
     }
 
@@ -74,5 +91,31 @@ public class LostProtectActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String pwdText = pwd.getText().toString().trim();
+        String pwdConfirmText = pwdConfirm.getText().toString().trim();
+        switch (v.getId()) {
+            case R.id.bt_ok:
+                if ("".equals(pwdText) || "".equals(pwdConfirmText)) {
+                    Log.i(TAG, "密码不能为空");
+                    Toast.makeText(this, "密码不能为空，请重新输入", Toast.LENGTH_LONG).show();
+                    return;
+                }else if (pwdText.equals(pwdConfirmText)) {
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("password", pwdConfirmText);
+                    editor.commit();
+                } else {
+                    Log.i(TAG, "密码不同，请重新输入");
+                    Toast.makeText(this, "密码不同，请重新输入", Toast.LENGTH_LONG).show();
+                    pwd.setText("");
+                    pwdConfirm.setText("");
+                    return;
+                }
+            case R.id.bt_cancel:
+                dialog.dismiss();
+        }
     }
 }
