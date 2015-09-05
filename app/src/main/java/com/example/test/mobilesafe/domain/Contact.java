dec1ad8c8ef4fdc223603d7d6acebb1e11b6d62c
michael <neo21anderson@gmail.com>
2015-09-05 15:41:15 +0800

    create a contact class
    create a contact class for use

---
+++ app/src/main/java/com/example/test/mobilesafe/domain/Contact.java
@@ -0,0 +1,25 @@
+package com.example.test.mobilesafe.domain;
+
+/**
+ * Created by test on 9/5/2015.
+ */
+public class Contact {
+    private String name;
+    private String number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
