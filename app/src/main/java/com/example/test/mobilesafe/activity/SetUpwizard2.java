5d65a94209657e73d66f600d90f8bb3f8b12ec39
michael <neo21anderson@gmail.com>
2015-09-05 15:26:40 +0800

    bind listener to the button
    bind listener to the button except the selectcontact button

--- app/src/main/java/com/example/test/mobilesafe/activity/SetUpwizard2.java
+++ app/src/main/java/com/example/test/mobilesafe/activity/SetUpwizard2.java
@@ -1,18 +1,35 @@
 package com.example.test.mobilesafe.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.test.mobilesafe.R;
 
-public class SetUpwizard2 extends AppCompatActivity {
-
+public class SetUpwizard2 extends AppCompatActivity implements View.OnClickListener{
+    private Button next;
+    private Button previous;
+    private Button selectContact;
+    private EditText contact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_upwizard2);
+
+        next = (Button) findViewById(R.id.bt_next);
+        previous = (Button) findViewById(R.id.bt_previous);
+
+        selectContact = (Button) findViewById(R.id.bt_select_contact);
+        contact = (EditText) findViewById(R.id.et_tel_number);
+
+        next.setOnClickListener(this);
+        previous.setOnClickListener(this);
+        selectContact.setOnClickListener(this);
     }
 
     @Override
@@ -35,5 +52,28 @@ public class SetUpwizard2 extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_next:
+                Intent intent = new Intent(this, SetUpwizard3.class);
+                finish();
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
+                break;
+            case R.id.bt_previous:
+                Intent intent1 = new Intent(this, SetUpwizard1.class);
+                finish();
+                startActivity(intent1);
+                overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
+                break;
+            case R.id.bt_select_contact:
+
+                break;
+            default:
+                break;
+        }
     }
 }
