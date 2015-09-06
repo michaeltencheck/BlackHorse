e62d1a04f83f6500ba15b12dbdcc7137dcf56cde
michael <neo21anderson@gmail.com>
2015-09-06 16:40:25 +0800

    create the contact service
    create the contact service

--- app/src/main/java/com/example/test/mobilesafe/engine/ContactInfoService.java
+++ app/src/main/java/com/example/test/mobilesafe/engine/ContactInfoService.java
@@ -28,13 +28,30 @@ public class ContactInfoService {
     public List<Contact> getContacts() {
         ContentResolver resolver = context.getContentResolver();
         Uri uri = Uri.parse("content://com.android.contacts/raw_contacts");
+        Uri dataUri = Uri.parse("content://com.android.contacts/data");
         Cursor cursor = resolver.query(uri, null, null, null, null);
         while (cursor.moveToNext()) {
             String id = cursor.getString(cursor.getColumnIndex("_id"));
             Log.i(TAG, id);
             String name = cursor.getString(cursor.getColumnIndex("display_name"));
             Log.i(TAG, name);
+            Cursor dataCursor =
+                    resolver.query(dataUri, null, "raw_contact_id=?", new String[]{id}, null);
+            while (dataCursor.moveToNext()) {
+                String number = dataCursor.getString(dataCursor.getColumnIndex("data1"));
+                Log.i(TAG, number);
+                String type = dataCursor.getString(dataCursor.getColumnIndex("mimetype"));
+                Log.i(TAG, type);
+                Contact con = new Contact();
+                if ("vnd.android.cursor.item/phone_v2".equals(type)) {
+                    con.setName(name);
+                    con.setNumber(number);
+                    contacts.add(con);
+                }
+            }
+            dataCursor.close();
         }
-        return null;
+        cursor.close();
+        return contacts;
     }
 }
