662eb88468826c72203e29820b5b4d873ccb93be
michael <neo21anderson@gmail.com>
2015-09-07 17:17:06 +0800

    save the safe number
    save the safe number with sharepreference and make a new translate
    animation for turning to the next activity

--- app/src/main/java/com/example/test/mobilesafe/activity/SetUpwizard2.java
+++ app/src/main/java/com/example/test/mobilesafe/activity/SetUpwizard2.java
@@ -1,22 +1,27 @@
 package com.example.test.mobilesafe.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.test.mobilesafe.R;
 import com.example.test.mobilesafe.engine.ContactInfoService;
 
 public class SetUpwizard2 extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = "SetUpwizard2";
     private Button next;
     private Button previous;
     private Button selectContact;
     private EditText contact;
+    private SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,7 @@ public class SetUpwizard2 extends AppCompatActivity implements View.OnClickListe
 
         next = (Button) findViewById(R.id.bt_next);
         previous = (Button) findViewById(R.id.bt_previous);
+        sp = getSharedPreferences("config", MODE_PRIVATE);
 
         selectContact = (Button) findViewById(R.id.bt_select_contact);
         contact = (EditText) findViewById(R.id.et_tel_number);
@@ -60,10 +66,20 @@ public class SetUpwizard2 extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt_next:
+                String text = contact.getText().toString().trim();
+                if ("".equals(text)) {
+                    Log.i(TAG, "安全号码不能为空");
+                    Toast.makeText(this, "安全号码不能为空，请重新选择", Toast.LENGTH_LONG).show();
+                    return;
+                } else {
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("safeNumber", text);
+                    editor.commit();
+                }
                 Intent intent = new Intent(this, SetUpwizard3.class);
                 finish();
                 startActivity(intent);
-                overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
+                overridePendingTransition(R.anim.anim_in_translate, R.anim.anim_out_translate);
                 break;
             case R.id.bt_previous:
                 Intent intent1 = new Intent(this, SetUpwizard1.class);
