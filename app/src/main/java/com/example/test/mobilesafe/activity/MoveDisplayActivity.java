4720be0eb0811b4527755881dc49f81db2280d77
michael <neo21anderson@gmail.com>
2015-09-19 19:04:38 +0800

    store the x&y of imageview for showing again
    store the x&y of imageview for showing again,besides make the
    introduction to be display in different place

--- app/src/main/java/com/example/test/mobilesafe/activity/MoveDisplayActivity.java
+++ app/src/main/java/com/example/test/mobilesafe/activity/MoveDisplayActivity.java
@@ -1,17 +1,22 @@
 package com.example.test.mobilesafe.activity;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.test.mobilesafe.R;
 
 public class MoveDisplayActivity extends AppCompatActivity implements View.OnTouchListener{
+    private static final String TAG = "MoveDisplayActivity";
     private TextView introduction1;
     private TextView introduction2;
     private ImageView displayLocation;
@@ -25,15 +30,35 @@ public class MoveDisplayActivity extends AppCompatActivity implements View.OnTou
     private int r;
     private int t;
     private int b;
+    private SharedPreferences.Editor editor;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_move_display);
 
         introduction1 = (TextView) findViewById(R.id.tv_md_explaination1);
         introduction2 = (TextView) findViewById(R.id.tv_md_explaination2);
+
         displayLocation = (ImageView) findViewById(R.id.iv_md_display);
+
+        editor = getSharedPreferences("config", MODE_PRIVATE).edit();
+        sp = getSharedPreferences("config", MODE_PRIVATE);
+
+        int lastX = sp.getInt("lastX", 0);
+        int lastY = sp.getInt("lastY", 0);
+        Log.i(TAG, "x = " + lastX);
+        Log.i(TAG, "y = " + lastY);
+        RelativeLayout.LayoutParams p = (RelativeLayout.LayoutParams) displayLocation.getLayoutParams();
+        p.leftMargin = lastX;
+        p.topMargin = lastY-28;
+        displayLocation.setLayoutParams(p);
+
+
+       /* displayLocation.layout(displayLocation.getLeft() + lastX, displayLocation.getTop() + lastY,
+                displayLocation.getRight() + lastX, displayLocation.getBottom() + lastY);*/
 
         displayLocation.setOnTouchListener(this);
     }
@@ -44,6 +69,19 @@ public class MoveDisplayActivity extends AppCompatActivity implements View.OnTou
         getMenuInflater().inflate(R.menu.menu_move_display, menu);
         return true;
     }
+
+    /*@Override
+    *//*protected void onResume() {
+        super.onResume();
+        int lastX = sp.getInt("lastX", 0);
+        int lastY = sp.getInt("lastY", 0);
+        Log.i(TAG, "x = " + lastX);
+        Log.i(TAG, "y = " + lastY);
+        RelativeLayout.LayoutParams p = (RelativeLayout.LayoutParams) displayLocation.getLayoutParams();
+        p.leftMargin = lastX;
+        p.topMargin = lastY;
+        displayLocation.setLayoutParams(p);
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -78,16 +116,26 @@ public class MoveDisplayActivity extends AppCompatActivity implements View.OnTou
                         r = displayLocation.getRight();
                         t = displayLocation.getTop();
                         b = displayLocation.getBottom();
-                        if (t > 900) {
-                            introduction1.setVisibility(View.VISIBLE);
-                            introduction2.setVisibility(View.INVISIBLE);
-                        } else if (t < 300) {
+
+                        if (t < 300) {
                             introduction1.setVisibility(View.INVISIBLE);
                             introduction2.setVisibility(View.VISIBLE);
+                        }else if (t > 900) {
+                            introduction1.setVisibility(View.VISIBLE);
+                            introduction2.setVisibility(View.INVISIBLE);
                         }
+
                         displayLocation.layout(l + dx, t + dy, r + dx, b + dy);
                         startX = endX;
                         startY = endY;
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        int reX = (int) event.getX();
+                        int reY = (int) event.getY();
+                        editor.putInt("lastX", startX-reX);
+                        editor.putInt("lastY", startY-reY);
+                        editor.commit();
                         break;
                     default:
                         break;
