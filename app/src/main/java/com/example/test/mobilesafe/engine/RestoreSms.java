a52d5fb5d5beb8b53890c1a72f0d0e2008d28fd0
michael <neo21anderson@gmail.com>
2015-09-26 17:38:40 +0800

    restore sms part2
    restore sms part2

--- app/src/main/java/com/example/test/mobilesafe/engine/RestoreSms.java
+++ app/src/main/java/com/example/test/mobilesafe/engine/RestoreSms.java
@@ -1,8 +1,10 @@
 package com.example.test.mobilesafe.engine;
 
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Environment;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -32,7 +34,26 @@ public class RestoreSms {
             xmlPullParserFactory.setNamespaceAware(true);
             XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
             xmlPullParser.setInput(fileInputStream,"utf-8");
-            
+            ContentResolver resolver = context.getContentResolver();
+            while (xmlPullParser.getEventType() != XmlPullParser.END_DOCUMENT) {
+                if ("sms".equals(xmlPullParser.getName())) {
+                    if ("address".equals(xmlPullParser.getName())) {
+                        String address = xmlPullParser.nextText();
+                        values.put("address", address);
+                    }else if ("date".equals(xmlPullParser.getName())) {
+                        String address = xmlPullParser.nextText();
+                        values.put("date", address);
+                    }else if ("type".equals(xmlPullParser.getName())) {
+                        String address = xmlPullParser.nextText();
+                        values.put("type", address);
+                    }else if ("body".equals(xmlPullParser.getName())) {
+                        String address = xmlPullParser.nextText();
+                        values.put("body", address);
+                    }
+                }
+                resolver.insert(Uri.parse("content://sms/"), values);
+                values = null;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
