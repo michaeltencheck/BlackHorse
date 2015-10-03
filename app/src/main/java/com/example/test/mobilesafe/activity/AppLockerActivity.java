98e043faf4c6e69006decb3c7a96212b115b706b
michael <neo21anderson@gmail.com>
2015-10-03 17:49:47 +0800

    show data on listview
    show data on listview

--- app/src/main/java/com/example/test/mobilesafe/activity/AppLockerActivity.java
+++ app/src/main/java/com/example/test/mobilesafe/activity/AppLockerActivity.java
@@ -1,18 +1,62 @@
 package com.example.test.mobilesafe.activity;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.example.test.mobilesafe.R;
+import com.example.test.mobilesafe.adapter.AppLockerAdapter;
+import com.example.test.mobilesafe.domain.AppInfo;
+import com.example.test.mobilesafe.engine.AppInfoAssist;
+
+import java.util.List;
 
 public class AppLockerActivity extends AppCompatActivity {
-
+    private static final int GET_DATA_SUCCESS = 222;
+    private AppLockerAdapter appLockerAdapter;
+    private List<AppInfo> appInfos;
+    private AppInfoAssist appInfoAssist;
+    private ListView listView;
+    private LinearLayout linearLayout;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == GET_DATA_SUCCESS) {
+                appLockerAdapter = new AppLockerAdapter(AppLockerActivity.this, appInfos);
+                listView.setAdapter(appLockerAdapter);
+                linearLayout.setVisibility(View.INVISIBLE);
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_locker);
+
+        appInfoAssist = new AppInfoAssist(this);
+        listView = (ListView) findViewById(R.id.lv_ala_appInfo);
+        linearLayout = (LinearLayout) findViewById(R.id.ll_ala);
+
+        initAppInfo();
+    }
+
+    private void initAppInfo() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                appInfos = appInfoAssist.getAppInfos();
+                Message msg = new Message();
+                msg.what = GET_DATA_SUCCESS;
+                handler.sendMessage(msg);
+            }
+        }).start();
     }
 
     @Override
