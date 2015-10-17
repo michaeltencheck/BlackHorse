d502bae9b0f6d6f33f9869e4ec884473ab1f5eb7
michael <neo21anderson@gmail.com>
2015-10-17 16:32:49 +0800

    process the adapter part1
    process the adapter part1

--- app/src/main/java/com/example/test/mobilesafe/adapter/FlowAdapter.java
+++ app/src/main/java/com/example/test/mobilesafe/adapter/FlowAdapter.java
@@ -1,6 +1,10 @@
 package com.example.test.mobilesafe.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -9,19 +13,50 @@ import android.widget.TextView;
 
 import com.example.test.mobilesafe.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by test on 10/17/2015.
  */
 public class FlowAdapter extends BaseAdapter{
     private Context context;
+    private List<Drawable> icons;
+    private List<String> names;
+/*    private List<String> mobileFlows;
+    private List<String> wifiFlows;*/
 
     public FlowAdapter(Context context) {
         this.context = context;
+
+        List<Drawable> icons1 = new ArrayList<>();
+        List<String> names1 = new ArrayList<>();
+        List<String> mobileFlows = new ArrayList<>();
+        List<String> wifiFlows = new ArrayList<>();
+
+        PackageManager pm = context.getPackageManager();
+
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.MAIN");
+        intent.addCategory("android.intent.category.LAUNCHER");
+
+        List<ResolveInfo> list = pm.queryIntentActivities(intent, PackageManager.GET_UNINSTALLED_PACKAGES);
+
+        for (ResolveInfo info : list) {
+            String name = (String) info.loadLabel(pm);
+            names1.add(name);
+
+            Drawable icon = info.loadIcon(pm);
+            icons1.add(icon);
+        }
+
+        this.icons = icons1;
+        this.names = names1;
     }
 
     @Override
     public int getCount() {
-        return 30;
+        return icons.size();
     }
 
     @Override
@@ -50,6 +85,8 @@ public class FlowAdapter extends BaseAdapter{
             view = convertView;
             viewHolder = (ViewHolder) view.getTag();
         }
+        viewHolder.appIcon.setImageDrawable(icons.get(position));
+        viewHolder.appName.setText(names.get(position));
         return view;
     }
 
