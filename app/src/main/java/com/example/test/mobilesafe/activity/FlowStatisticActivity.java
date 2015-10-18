8d7b0092037413396780920085e772f864272721
michael <neo21anderson@gmail.com>
2015-10-18 14:16:38 +0800

    get the total flow of 3g and wifi
    get the total flow of 3g and wifi

--- app/src/main/java/com/example/test/mobilesafe/activity/FlowStatisticActivity.java
+++ app/src/main/java/com/example/test/mobilesafe/activity/FlowStatisticActivity.java
@@ -1,5 +1,6 @@
 package com.example.test.mobilesafe.activity;
 
+import android.net.TrafficStats;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -9,18 +10,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.test.mobilesafe.R;
 import com.example.test.mobilesafe.adapter.FlowAdapter;
+import com.example.test.mobilesafe.util.DecimalFormater;
 
 public class FlowStatisticActivity extends AppCompatActivity {
     private LinearLayout layout;
     private ListView listView;
+    private TextView mobile, wifi;
     private FlowAdapter adapter;
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+
+            long mobileFlow = TrafficStats.getMobileRxBytes() + TrafficStats.getMobileRxPackets()
+                    + TrafficStats.getMobileTxBytes() + TrafficStats.getMobileTxPackets();
+            long wifiFlow = TrafficStats.getTotalRxBytes() + TrafficStats.getTotalRxPackets()
+                    + TrafficStats.getTotalTxBytes() + TrafficStats.getTotalTxPackets()-mobileFlow;
+
+            mobile.setText(DecimalFormater.getNumber(mobileFlow));
+            wifi.setText(DecimalFormater.getNumber(wifiFlow));
+
             listView.setAdapter(adapter);
             layout.setVisibility(View.INVISIBLE);
         }
@@ -33,6 +46,9 @@ public class FlowStatisticActivity extends AppCompatActivity {
 
         layout = (LinearLayout) findViewById(R.id.ll_afs_progress);
         listView = (ListView) findViewById(R.id.lv_afs_flowDetail);
+
+        mobile = (TextView) findViewById(R.id.tv_afs_mobileFlow);
+        wifi = (TextView) findViewById(R.id.tv_afs_wifiFlow);
 
         getAdapter();
     }
